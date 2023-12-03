a84ac128be91824b48620dee971b86089bc2f03a
ммđ αмıи ᴾᴴᴾ <51942680+Mr-Basirnia@users.noreply.github.com>
2023-12-03 17:27:22 +0330

    ✨ feat(migration): complete migrations

---
+++ database/migrations/2023_12_03_163952_create_events_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\City;
+use App\Models\User;
+use App\Models\Country;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->longText('description');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('image');
+            $table->string('address');
+            $table->integer('num_tickets');
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Country::class)->constrained();
+            $table->foreignIdFor(City::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
