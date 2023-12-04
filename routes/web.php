b6e8db26fc8d3b7e052070953c8d8326b911c43d
ммđ αмıи ᴾᴴᴾ <51942680+Mr-Basirnia@users.noreply.github.com>
2023-12-04 13:20:17 +0330

    ✨ feat(controllers): route-controllers
    define new route and controllers for events  and galleries.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,9 +25,14 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::resource('/events', EventController::class);
+    Route::resource('/galleries', GalleryController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
