a1f01d95ad2d928cb332d50348ab9fedcbd36751
ммđ αмıи ᴾᴴᴾ <51942680+Mr-Basirnia@users.noreply.github.com>
2023-12-05 16:10:04 +0330

    ✨ feat(gallery): crud for gallery
    handle crud for galleries.

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Gallery;
 use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\Gallery\CreateGalleryRequest;
+use App\Http\Requests\Gallery\UpdateGalleryRequest;
 
 class GalleryController extends Controller
 {
@@ -12,23 +16,33 @@ class GalleryController extends Controller
      */
     public function index(): View
     {
-        return view('galleries.index');
+        $galleries = auth()->user()->galleries;
+        return view('galleries.index', compact('galleries'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('galleries.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateGalleryRequest $request): RedirectResponse
     {
-        //
+        if ($request->hasFile('image')) {
+            $data = $request->validated();
+            $data['image'] = $request->file('image')->store('galleries', 'public');
+
+            auth()->user()->galleries()->create($data);
+
+            return to_route('galleries.index');
+        }
+
+        return back();
     }
 
     /**
@@ -42,24 +56,35 @@ class GalleryController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Gallery $gallery): View
     {
-        //
+        return view('galleries.edit', compact('gallery'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateGalleryRequest $request, Gallery $gallery): RedirectResponse
     {
-        //
+        $data = $request->validated();
+        if ($request->hasFile('image')) {
+            Storage::disk('public')->delete($gallery->image);
+            $data['image'] = $request->file('image')->store('galleries', 'public');
+        }
+
+        $gallery->update($data);
+
+        return to_route('galleries.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Gallery $gallery): RedirectResponse
     {
-        //
+        Storage::disk('public')->delete($gallery->image);
+        $gallery->delete();
+
+        return to_route('galleries.index');
     }
 }
